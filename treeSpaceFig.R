90d7e3bd3582d792cfd2ecef655c2eb0df18434f
Leo <58927485+LeoFeatherstone@users.noreply.github.com>
2022-09-01 16:17:48 +1000

    data concept

--- treeSpaceFig.R
+++ treeSpaceFig.R
@@ -4,26 +4,46 @@ library(combinat)
 
 # generating all base topologies for a tree with 3 tips
 set.seed(1234)
-t1 <- rtopology(3, rooted = TRUE, tip.label = NULL, br = 1)
-t1$edge.length <- c(1, 3, 2, 1) # giving order to tips
-t1$tip.labels <- LETTERS[1:3]
+t <- rtopology(3, rooted = TRUE, tip.label = NULL, br = 1)
+t$edge.length <- c(1, 3, 2, 1) # giving order to tips
+t$tip.label <- LETTERS[1:3]
 
-t2 <- t1
-t2$edge[, 2] <- c(5, 3, 2, 1)
-t3 <- t
-t3$edge[, 2] <- c(5, 1, 3, 2)
+trees <- list()
+class(trees) <- "multiPhylo"
+for (i in 1:3){
+    trees[[i]] <- t
+    }
 
-# plot all base topologies
-par(mfrow = c(1, 3))
-plot(t1, direction = "downwards")
-tiplabels(cex=3)
-plot(t2, direction = "downwards")
-tiplabels(cex=3)
-plot(t3, direction = "downwards")
-tiplabels(cex=3)
+# list of base topologies
+tops <- list()
+for (i in seq_along(combn(1:3, 2)[1, ])){
+    tops[[i]] <- union(combn(1:3, 2)[, i], 1:3)
+}
 
-# all possible tip name orders
-permn(LETTERS[1:3])
+for (i in seq_along(trees)){
+    trees[[i]]$edge[2:4, 2] <- tops[[i]]
+}
 
-# generate all possibilities with just sequence information
-seq <- list()
+all_tree <- list()
+class(all_tree) <- "multiPhylo"
+col <- vector()
+for (i in seq_along(trees)){
+    tmp <- trees[[i]]
+    for (j in seq_along(permn(c(1, 2, 4)))){
+        tmp$edge.length[-1] <- permn(c(1, 2.5, 4))[[j]]
+        all_tree[[(i - 1) * max(seq_along(permn(c(1, 2, 4)))) + j]] <- tmp
+        if (((i - 1) * max(seq_along(permn(c(1, 2, 4)))) + j) %in% c(1, 8, 17)) {
+            col <- c(col, 'blue')
+            } else {
+                col <- c(col, 'black')
+            }
+    }
+}
+
+#plot
+tiff("./figures/egTreeSpace.tiff", width = 4, height = 6, units = 'in', res = 300)
+    par(mfrow = c(6, 3), mar = rep(1, 4))
+    for (i in seq_along(all_tree)){
+        plot.phylo(all_tree[[i]], edge.color = col[i], edge.width = 3, cex = 0.8)
+    }
+dev.off()
