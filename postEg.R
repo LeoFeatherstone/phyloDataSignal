3d351fad34eca42aeca394d47fae4141f91e4b94
Leo <58927485+LeoFeatherstone@users.noreply.github.com>
2022-09-30 17:40:26 +1000

    Fig updated

--- postEg.R
+++ postEg.R
@@ -1,3 +1,6 @@
+### Script produces figure for example posteriors.
+### In future add app to visualise all
+
 library(ggplot2)
 library(viridis)
 library(reshape)
@@ -5,64 +8,38 @@ library(cowplot)
 library(tidyverse)
 
 path <- getwd()
-figPath <- paste0(path, '/figures/')
+figPath <- paste0(path, "/figures/")
 
 # get data
-load('wassersteinData.RData')
-load('postR0.RData')
+load("wassersteinData.RData")
 
-# making plot data
-metaData <- as.data.frame(cbind(rate, sampProp, wassersteinClass, names(wassersteinClass)))
-colnames(metaData) <- c('rate', 'sampProp', 'wassersteinClass', 'id')
+load("postR0.RData")
 
-post <- bind_rows(chains[5:12], .id='id') %>% left_join(metaData) %>% 
-pivot_longer(ends_with("Data"), values_to='R0', names_to='post', values_drop_na=T)
+# selecting plot data
+post <- bind_rows(chains[7:12], .id = "id") %>%
+left_join(wassersteinData) %>%
+pivot_longer(ends_with("Data"), values_to = "R0", names_to = "type", values_drop_na = TRUE)
 
-# looking for a tree example with an edge case
-# double checking this corresponds to the same idea in wasserstein metric
-candidates <- vector()
-diff <- vector()
-for (i in 1:length(chains)) { 
-	d <- transport::wasserstein1d(na.omit(chains[[i]]$dateData), na.omit(chains[[i]]$fullData))
-	s <- transport::wasserstein1d(na.omit(chains[[i]]$seqData), na.omit(chains[[i]]$fullData))
-	sd <- transport::wasserstein1d(na.omit(chains[[i]]$seqData), na.omit(chains[[i]]$dateData))
-
-	if((sd<s && sd<d)) {
-		print(paste(i, ':', 'd', d, 's', s, 'sd', sd))}
-}
-# i=32 good candidate
-# which(grepl(names(chains), pattern='t17p'))
-#  names(chains)[32] "t8p1r1e-03"
-#  names(chains)[c(5:8,32)] ==> pick 5,7,8,66
-# plotting that with the cleatest diff
-post <- bind_rows(chains[c(5:7,32)], .id='id') %>% left_join(metaData) %>% 
-pivot_longer(ends_with("Data"), values_to='R0', names_to='post', values_drop_na=T)
-
-id.labs <- c(rep('Tree 2',3), 'Tree 8')
-names(id.labs) <- names(chains[c(5:7,32)])
-rate.labs <- c(('0.00001 (subs/site/year)'), ('0.001 (subs/site/year)'))
-#rate.labs <- c(expression('10'^{-3}~'(subs/site/year)'), expression('10'^{-5}~'(subs/site/year)'))
+# labels for facets
+rate.labs <- c(("Simulation rate: 10^-5 (subs/site/year)"), ("Simulation rate: 10^-3 (subs/site/year)"))
 names(rate.labs) <- unique(post$rate)
-sampProp.labs <- c('p=0.5', 'p=1.0')
-names(sampProp.labs) <- unique(post$sampProp)
+sampProp.labs <- c("Simulation sampling proportion = 0.5", "Simulation sampling proportion = 1.0")
+names(sampProp.labs) <- unique(post$sampProp)[-1]
 
-pdf(paste0(figPath, 'postEg.pdf'), useDingbats=F, height=10, width=13)
-	ggplot(post, aes(x=R0, fill=(post), label=wassersteinClass)) + geom_density() +
-			labs(x=expression('Posterior R'[0]), y='Density') +
-			scale_fill_manual(values=alpha(viridis(4),0.5), 
-				labels=c('Date Data', 'Full Data', 'Marginal Prior', 'Sequence Data')) +
-			geom_text(x=4.5, y=1, check_overlap=T) +
-			facet_wrap(id~rate~sampProp,
-				labeller = labeller(id = id.labs, rate = rate.labs, sampProp=sampProp.labs),
-				nrow=2) +
-			theme_bw() +
-			theme(legend.position='bottom',
-				legend.title=element_blank()) 
+tiff(file = paste0(figPath, "postEg.tiff"))
+ ggplot(subset(post, sampProp != 0.05), aes(x = R0, fill = type, label = class)) + geom_density() +
+ labs(x = expression("Posterior R"[0]), y = "Density") +
+ scale_fill_manual(values = alpha(viridis(4), 0.5), 
+ labels = c("Date Data", "Full Data", "Marginal Prior", "Sequence Data")) +
+ geom_text(x = 1.5, y = 2, check_overlap = TRUE, size = 5) +
+ xlim(1, 4) +
+ facet_wrap(~rate + sampProp,
+  labeller = labeller(rate = rate.labs, sampProp = sampProp.labs),
+  nrow = 2, scales = "free_y") +
+ theme_bw() +
+ theme(legend.position="bottom",
+  legend.title = element_blank(),
+  legend.text = element_text(size = 12),
+  strip.text = element_text(size = 12),
+  axis.title = element_text(size = 12))
 dev.off()
-
-
-
-
-
-
-
