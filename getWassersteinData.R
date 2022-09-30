ec3b7bd14159b3ceb2bfb24ff298ef14c5b6b981
Leo <58927485+LeoFeatherstone@users.noreply.github.com>
2022-09-30 15:01:51 +1000

    wrangle wasserstein data

---
+++ getWassersteinData.R
@@ -0,0 +1,31 @@
+### Script calculates wasserstein distance and classifies
+library(transport)
+library(tidyverse)
+
+# clasifying by wasserstein statistic
+load("postR0.RData")
+
+# function for distance from posterior with full data
+wassersteinCompare <- function(df) {
+ op <- c(wasserstein1d(na.omit(df$fullData), na.omit(df$dateData)),
+ wasserstein1d(na.omit(df$fullData), na.omit(df$seqData)),
+ wasserstein1d(na.omit(df$fullData), na.omit(df$noData)))
+
+ names(op) <- c("wd", "ws", "wn")
+ return(op)
+}
+
+# Wrangle Wasserstein distances into data frame
+tmp <- lapply(chains, function(x) wassersteinCompare(x))
+wassersteinData <- as.data.frame(do.call("rbind", tmp))
+
+# classify
+wassersteinData <- wassersteinData %>%
+mutate(
+ class = case_when(
+  wd < ws ~ "Date-Driven",
+  ws < wd ~ "Seq-Driven"
+ )
+)
+
+save(wassersteinData, file = "wassersteinData.RData")
